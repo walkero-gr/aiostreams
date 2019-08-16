04416af563b74717e34ef1a20e3e4b2635b60701
George Sokianos <walkero@gmail.com>
2019-08-16 15:41:53 +0100

    Added m3u8 manipulation. This needs tobe tested with the demo files on Python 2.5.6. Added logic to get a stream url based on the preferred quality. Added argument to set the quality.

--- twitch.py
+++ twitch.py
@@ -5,9 +5,22 @@ import simplejson as json
 # from pprint import pprint
 from urllib2 import Request, urlopen, URLError
 from random import random
-# import m3u8
+import m3u8
 
 clientId = "k5y7u3ntz5llxu22gstxyfxlwcz10v"
+
+videoPlayer = "APPDIR:ffmpeg"
+streamPlayer = "APPDIR:ffplay"
+
+qualityWeight = [
+	"480p30",
+	"360p30",
+	"160p30",
+	"audio_only",
+	"chunked",
+	"720p60",
+	"720p30"
+]
 
 _url_re = re.compile(r"""
     http(s)?://
@@ -51,7 +64,7 @@ class twitchAPIHandler:
 		except URLError, e:
 			print e.reason
 		
-		return
+		return None
 
 	def getChannelInfoByName(self, channelName):
 		endpoint = "kraken/channels/%s.json" % (channelName)
@@ -92,7 +105,7 @@ class usherHandler:
 	def __init__(self):
 		self.baseurl = 'https://usher.ttvnw.net'
 
-		return
+		return None
 
 	def call(self, endpoint, query):
 		query_args = urllib.urlencode(query)
@@ -108,7 +121,7 @@ class usherHandler:
 		except URLError, e:
 			print e.reason
 		
-		return
+		return None
 
 	def getChannelStreams(self, channelName, sig, token):
 		endpoint = "api/channel/hls/%s" % (channelName)
@@ -159,7 +172,36 @@ class helpersHandler:
 		if (types['videos_id']):
 			return {'type': 'video', 'id': types['videos_id']}
 
-		return 
+		return None
+
+	def m3u8GetModel(self, data):
+		m3u8Data = m3u8.loads(data)
+		return m3u8Data.data
+	
+	def m3u8GetPlaylists(self, data):
+		m3u8Model = self.m3u8GetModel(data)
+		# print m3u8Model['playlists']
+		return m3u8Model['playlists']
+	
+	def m3u8GetMedia(self, data):
+		m3u8Model = self.m3u8GetModel(data)
+		# print m3u8Model['media']
+		return m3u8Model['media']
+
+	def getPrefferedVideoURL(self, data):
+		playlists = self.m3u8GetPlaylists(data)
+
+		weightCnt = 0
+		for quality in qualityWeight:
+			for playlist in playlists:
+				if (quality == playlist['stream_info']['video']):
+					# print quality
+					# print playlist['stream_info']['video']
+					# print playlist['uri']
+					# print "---------------------------"
+					# break
+					return playlist['uri']
+
 
 def main(argv):
 	twitchApi = twitchAPIHandler()
@@ -170,38 +212,52 @@ def main(argv):
 	# Parse the arguments
 	argParser = argparse.ArgumentParser(description='This is a python script that uses twitch.tv API to get information about channels/videos for AmigaOS 4.1 and above.')
 	argParser.add_argument('-u', '--url', action='store', dest='url', help='The video/channel url from twitch.tv')
+	argParser.add_argument('-q', '--quality', action='store', dest='quality', help='Set the preffered video quality. This is optional. If not set or if it is not available the default quality weight will be used.')
 	args = argParser.parse_args()
 
 	if (args.url):
 		twitchURL = args.url
 		video = helpers.getVideoType(args.url)
+	if (args.quality):
+		qualityWeight.insert(0, args.quality)
+
 
 	if (video['type'] == 'channel'):
 		channelName = video['id']
 			
 		streams = twitchApi.getStreamsByChannel(channelName)
-		if (streams['stream']):
-			if (streams['stream']['stream_type'] == 'live'):
-				accessToken = twitchApi.getAccessTokenByChannel(channelName)
-				# usherApi.getChannelStreams(channelName, accessToken['sig'], accessToken['token'])
-		else:
-			print "There is no Live stream for the channel: %s" % (channelName)
+		if (streams):
+			if (streams['stream']):
+				if (streams['stream']['stream_type'] == 'live'):
+					accessToken = twitchApi.getAccessTokenByChannel(channelName)
+					usherApi.getChannelStreams(channelName, accessToken['sig'], accessToken['token'])
+			else:
+				print "There is no Live stream for the channel: %s" % (channelName)
 
 
 	if (video['type'] == 'video'):
 		videoId = video['id']
 
 		streams = twitchApi.getVideoInfoByID(videoId)
-		if (streams['viewable'] == 'public'):
-			accessToken = twitchApi.getAccessTokenByVideo(videoId)
-			usherApi.getVideoStreams(videoId, accessToken['sig'], accessToken['token'])
+		if (streams):
+			if (streams['viewable'] == 'public'):
+				accessToken = twitchApi.getAccessTokenByVideo(videoId)
+				# usherApi.getVideoStreams(videoId, accessToken['sig'], accessToken['token'])
+		else:
+			print "There is no video available with ID: %s" % (videoId)
 
 
+	# TODO: The following code is for testing the m3u8 parser with the demo files
+	# f = open("demoLives.m3u8", "r")
+	# print(f.read()) 
+	# uri = helpers.getPrefferedVideoURL(f.read())
+	# f.close()
+	# print uri
 
 
-	# # TODO: The following list is temporary for tests. This will be removed
-	# # https://www.twitch.tv/bnepac
-	# # https://www.twitch.tv/videos/464055415
+	# TODO: The following list is temporary for tests. This will be removed
+	# https://www.twitch.tv/bnepac
+	# https://www.twitch.tv/videos/464055415
 	# channels = [
 	# 	"riotgamesoce",
 	# 	"amigabill",
