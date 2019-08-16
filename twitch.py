f4f7860d6303a9c3709ceba8a45b60d6025fc335
George Sokianos <walkero@gmail.com>
2019-08-16 13:03:21 +0100

    Major reconstruction of the twitch script

--- twitch.py
+++ twitch.py
@@ -1,79 +1,111 @@
 #!python
 
-import urllib2, sys, subprocess, textwrap, datetime, time, argparse
-from pprint import pprint
+import urllib, urllib2, sys, subprocess, textwrap, datetime, time, argparse
 import simplejson as json
-#import m3u8
+# from pprint import pprint
+from urllib2 import Request, urlopen, URLError
+from random import random
+# import m3u8
 
-clientId = "pwkzresl8kj2rdj6g7bvxl9ys1wly3j"
+clientId = "k5y7u3ntz5llxu22gstxyfxlwcz10v"
 
 
-def apiGetChannelInfoByName(channelName):
-	#url = "https://%(te)s.teamwork.com/tasks/%(cid)s.json" % {'te': "api", 'cid': clientId}
-	#url = "https://%s.teamwork.com/tasks/%s.json" % ("api", clientId)
-	url = "https://api.twitch.tv/kraken/channels/%s.json" % (channelName)
-	print(url)
-	#headers = urllib3.util.make_headers(basic_auth=key + ":xxx")
-	#request = http.request('GET', url, headers=headers)
-	request = urllib2.Request(url)
-	request.add_header('Accept', 'application/vnd.twitchtv.v4+json')
-	request.add_header('Client-ID', clientId)
-	response = urllib2.urlopen(request)
-	
-	#print(response.read())
-	#response = request.status
-	retData = json.load(response)
-	print(retData['status'])
-	return 0 #retData
+class twitchAPIHandler:
+	def __init__(self):
+		self.baseurl = 'https://api.twitch.tv'
 
-def apiGetStreamsByChannel(channelName):
-	url = "https://api.twitch.tv/kraken/streams/%s.json" % (channelName)
-	print(url)
-	request = urllib2.Request(url)
-	request.add_header('Accept', 'application/vnd.twitchtv.v4+json')
-	request.add_header('Client-ID', clientId)
-	response = urllib2.urlopen(request)
-	
-	retData = json.load(response)
-	print(retData)
-	return 0 #retData
+		return
 
-def apiGetAccessTokenByChannel(channelName):
-	url = "https://api.twitch.tv/api/channels/%s/access_token.json" % (channelName)
-	print(url)
-	request = urllib2.Request(url)
-	request.add_header('Accept', 'application/vnd.twitchtv.v4+json')
-	request.add_header('Client-ID', clientId)
-	response = urllib2.urlopen(request)
-	
-	retData = json.load(response)
-	# print(retData['token'])
-	# print(retData['sig'])
-	return retData
+	def call(self, endpoint):
+		url = "%s/%s" % (self.baseurl, endpoint)
+		request = urllib2.Request(url)
+		request.add_header('Accept', 'application/vnd.twitchtv.v4+json')
+		request.add_header('Client-ID', clientId)
+		try:
+			response = urllib2.urlopen(request)
+			retData = json.load(response)
+			response.close()
+			return retData
+		except URLError, e:
+			print e.reason
+		
+		return
 
-def usherGetChannelStreams(channelName, sig, token):
-	url = "https://usher.ttvnw.net/api/channel/hls/%s?player=twitchweb&type=any&allow_source=true&allow_audio_only=true&allow_spectre=false&p=100&nauthsig=%s&&nauth=%s" % (channelName, sig, token)
-	print(url)
-	request = urllib2.Request(url)
-	request.add_header('Accept', 'application/vnd.twitchtv.v4+json')
-	request.add_header('Client-ID', clientId)
-	response = urllib2.urlopen(request)
-	
-	#retData = json.load(response)
-	print(response.read())
-	# print(retData['sig'])
-	return 0 # retData
+	def getChannelInfoByName(self, channelName):
+		endpoint = "kraken/channels/%s.json" % (channelName)
+		retData = self.call(endpoint)
+		# print retData
+
+		return retData
+
+	def getStreamsByChannel(self, channelName):
+		endpoint = "kraken/streams/%s.json" % (channelName)
+		retData = self.call(endpoint)
+		# print retData
+
+		return retData
+
+	def getAccessTokenByChannel(self, channelName):
+		endpoint = "api/channels/%s/access_token.json" % (channelName)
+		retData = self.call(endpoint)
+		# print retData
+
+		return retData
+
+class usherHandler:
+	def __init__(self):
+		self.baseurl = 'https://usher.ttvnw.net'
+
+		return
+
+	def call(self, endpoint, query):
+		query_args = urllib.urlencode(query)
+		url = "%s/%s" % (self.baseurl, endpoint)
+		print(url)
+		request = urllib2.Request(url, query_args)
+
+		try:
+			response = urllib2.urlopen(request)
+			retData = json.load(response)
+			response.close()
+			return retData
+		except URLError, e:
+			print e.reason
+		
+		return
+
+	def getChannelStreams(self, channelName, sig, token):
+		endpoint = "api/channel/hls/%s" % (channelName)
+		query = {
+			"player": "twitchweb",
+			"type": "any",
+			"allow_source": "true",
+			"allow_audio_only": "true",
+			"allow_spectre": "false",
+			"p": int(random() * 999999),
+			"nauthsig": sig,
+			"nauth": token
+		}
+		retData = self.call(endpoint, query)
+		print retData
+
+		return # retData
 
 def main(argv):
-	channelName = "hayesmaker64"
+	twitchApi = twitchAPIHandler()
+	usherApi = usherHandler()
+
+	# TODO: Replace this with code to parse a twitch url 
+	channelName = "riotgamesoce"
+
 	# Parse the arguments
-	argParser = argparse.ArgumentParser(description='This is a python script that can be used to get information from Teamwork Projects Management. You can find more info at https://github.com/walkero-gr/tw')
+	argParser = argparse.ArgumentParser(description='This is a python script that uses twitch.tv API to get information about channels/videos for AmigaOS 4.1 and above.')
 	#argParser.add_argument('-p', '--project', action='store', dest='project_name', help='set the project name')
 	args = argParser.parse_args()
 	
-	# print(json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}]))
-	accessToken = apiGetAccessTokenByChannel(channelName)
-	usherGetChannelStreams(channelName, accessToken['sig'], accessToken['token'])
+	# twitchApi.getStreamsByChannel(channelName)
+	accessToken = twitchApi.getAccessTokenByChannel(channelName)
+	usherApi.getChannelStreams(channelName, accessToken['sig'], accessToken['token'])
 	
 	sys.exit()
 
