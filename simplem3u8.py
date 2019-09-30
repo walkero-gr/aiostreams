0568c7bf95ac784a990ab850ab11a89bb69838bf
George Sokianos <walkero@gmail.com>
2019-09-30 16:20:24 +0100

    Did a fix on simplem3u8 attribute split to fix attrs that have more than one "=" in value

--- simplem3u8.py
+++ simplem3u8.py
@@ -3,36 +3,35 @@ import re
 ATTRIBUTELISTPATTERN = re.compile(r'''((?:[^,"']|"[^"]*"|'[^']*')+)''')
 
 class parseHandler:
-    def parse(self, m3u8Data):
-        listCnt = 0
-        retList = {}
-        retList[listCnt] = {}
+	def parse(self, m3u8Data):
+		listCnt = 0
+		retList = {}
+		retList[listCnt] = {}
 
-        for line in m3u8Data.splitlines():
-            if (line.startswith("#EXT-X-MEDIA:")):
+		for line in m3u8Data.splitlines():
+			if (line.startswith("#EXT-X-MEDIA:")):
 				params = self.parseExt(line, "#EXT-X-MEDIA:")
 
 				for attribute in params:
-					attr, val = attribute.split("=")
+					attr, val = attribute.split("=", 1)
 					retList[listCnt][attr.lower()] = val.lower()
 
-            if (line.startswith("#EXT-X-STREAM-INF:")):
-                params = self.parseExt(line, "#EXT-X-STREAM-INF:")
+			if (line.startswith("#EXT-X-STREAM-INF:")):
+				params = self.parseExt(line, "#EXT-X-STREAM-INF:")
+				for attribute in params:
+					attr, val = attribute.split("=", 1)
+					val = val.replace('"', "")
+					retList[listCnt][attr.lower()] = val.lower()
 
-                for attribute in params:
-                    attr, val = attribute.split("=")
-                    val = val.replace('"', "")
-                    retList[listCnt][attr.lower()] = val.lower()
+			if (line.startswith(('https://', 'http://', '../'))):
+				retList[listCnt]['uri'] = line
+				listCnt += 1
+				retList[listCnt] = {}
 
-            if (line.startswith(('https://', 'http://', '../'))):
-                retList[listCnt]['uri'] = line
-                listCnt += 1
-                retList[listCnt] = {}
+		return retList
 
-        return retList
-    
-    def parseExt(self, line, tag):
-        extMedia = line.replace(tag, "")
-        extMedia = extMedia.replace("\n", "")
+	def parseExt(self, line, tag):
+		extMedia = line.replace(tag, "")
+		extMedia = extMedia.replace("\n", "")
 
-        return ATTRIBUTELISTPATTERN.split(extMedia)[1::2]
+		return ATTRIBUTELISTPATTERN.split(extMedia)[1::2]
